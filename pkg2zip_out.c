17aa520d98aaa4d2271d9edfceeb665de3f2b501
lusid1 <sean_hatfield@hotmail.com>
2026-08-03 10:48:20 -0700

    Fix memory issue by removing strdup in out_add_parent

--- pkg2zip_out.c
+++ pkg2zip_out.c
@@ -40,7 +40,7 @@ void out_add_folder(const char* path)
 void out_add_parent(const char* path)
 {
     char parent[1024];
-    char* lastslash = strrchr(strdup(path), '/');
+    char* lastslash = strrchr((char*)path, '/');
     if (lastslash != NULL)
     {
         snprintf(parent, strlen(path)-strlen(lastslash)+1, "%s", path);
